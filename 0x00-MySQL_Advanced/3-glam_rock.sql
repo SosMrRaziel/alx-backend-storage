8282f7cec7d7ec4347a883ce7fdfe18c52417392
SoSMrRaziel <lbt_alh@hotmail.fr>
2024-06-12 22:14:23 +0100

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,4 @@
 -- Go to the glam_rock table and select the band_name and formed columns.
-SELECT band_name,
-	ifnull(split, YEAR(CURDATE())) - formed as lifespan
-FROM metal_bands
-WHERE style LIKE "%Glam rock%"
-ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
+FROM metal_bands WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan  DESC;
