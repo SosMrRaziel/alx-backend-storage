ab8cd5113244431cdfa99f4e609b381e4ab36206
SosMrRaziel <lbt_alh@hotmail.fr>
2024-06-18 20:15:34 +0100

    Change school topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,6 +2,7 @@
 """ a Python function that changes all topics of a school document based on the name """
 from pymongo import MongoClient
 
+
 def update_topics(mongo_collection, name, topics):
     """ Update the topics of a school document based on the name """
     return mongo_collection.update_many(
