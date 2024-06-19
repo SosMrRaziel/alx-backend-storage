d5292fa9c2c96450ea92a82b533581198c008ecf
SosMrRaziel <lbt_alh@hotmail.fr>
2024-06-19 23:58:05 +0100

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Provides some stats about Nginx logs stored in Redis """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -22,3 +22,26 @@ class Cache:
         key = str(uuid.uuid4())  # Generate a random key
         self._redis.set(key, data)  # Store data in Redis
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[
+                str, bytes, int, float, None]:
+        """
+        Retrieves data from Redis using the provided key.
+        Optionally applies the conversion function `fn` to the retrieved data.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieves data from Redis as a UTF-8 string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieves data from Redis as an integer.
+        """
+        return self.get(key, fn=int)
